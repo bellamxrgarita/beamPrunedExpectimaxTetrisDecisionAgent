1e413f01e2f9e861cecb5cb0548c73a9149f6186
Bella Dayrit <belladayrit@Bellas-MacBook-Air.local>
2025-12-31 14:20:18 -0500

    Introduced second beamsearch expectimax hybrid

---
+++ src/beamPrunedExpectimaxAgent.py
@@ -0,0 +1,125 @@
+from TetrisStateSpace import TetrisStateSpace
+import random
+import sys
+import traceback
+
+
+HEIGHT_WEIGHT = -0.51
+LINES_WEIGHT = 0.76
+HOLES_WEIGHT = -0.36
+BUMPINESS_WEIGHT = -0.18
+
+
+class beamPrunedExpectimaxAgent:
+    def __init__(self, agentName: str, beam_width = 2):
+        self.beam_width = beam_width
+        self.agentName = agentName
+
+    # This needed to be adjusted to simulate the next
+    def getAction(self, currGameState: TetrisStateSpace, depth: int = 2):
+        #loops over all potential moves and returns the one with the highest score
+        possibleMoves = currGameState.legal_placements()
+
+        if not possibleMoves:
+            return None
+
+        bestScore = -float("inf")
+        bestMove = None
+
+        for (col, orientation) in possibleMoves:
+            # but expectiMax now properly alternates max/chance and uses depth
+            score = self.expectiMax(currGameState, depth, (col, orientation), False)
+            ##print(score)
+            if score > bestScore:
+                bestScore = score
+                bestMove = (col, orientation)
+            
+        return bestMove
+    
+
+    # the current gamestate actually has the piece that we are supposed to put down stored in the queue
+    # the queue contains the next 3 pieces that will be placed by the player
+    # should this be modified to actualy take those 3 next pieces?
+    def expectiMax(self, gameState: TetrisStateSpace, depth: int, action, isMaxAgent: bool):
+    # Base case: either game over or search depth exhausted
+        if gameState.is_terminal() or depth <= 0:
+            return self.evaluationFunction(gameState)
+
+        if isMaxAgent:
+            legal_moves = gameState.legal_placements()
+
+            if not legal_moves:
+                return self.evaluationFunction(gameState)
+
+            # Search one ahead
+            scored_moves = []
+
+            for (col, orientation) in legal_moves:
+                next_state = gameState.place_piece(col, orientation, "O")
+                h = self.evaluationFunction(next_state)
+                scored_moves.append((h, (col, orientation)))
+
+            # Keep only top self.beam_width moves
+            scored_moves.sort(key=lambda x: x[0], reverse=True)
+            pruned_moves = [move for (_, move) in scored_moves[:self.beam_width]]
+
+            maxScore = -float("inf")
+            for (col, orientation) in pruned_moves:
+                # Next layer is a chance node, depth decreases
+                score = self.expectiMax(gameState, depth - 1, (col, orientation), isMaxAgent=False)
+                maxScore = max(maxScore, score)
+
+            return maxScore
+        else:
+            col, orientation = action
+            expectedScore = 0.0
+            num_pieces = len(gameState.VALID_SHAPES)
+
+            for piece in gameState.VALID_SHAPES:
+                newState = gameState.place_piece(col, orientation, piece)
+                # Next layer is a max node, depth also decreases here
+                score = self.expectiMax(newState, depth - 1, None, isMaxAgent=True)
+                expectedScore += score / num_pieces
+
+        return expectedScore
+
+    
+    # Referencing Pierre Dellacherie's Algorithm
+    # modify this to also make this take the queue's "fit" to the board
+    def evaluationFunction(self, currGameState: TetrisStateSpace):
+        board = currGameState.board
+        rows = len(board)
+        cols = len(board[0])
+
+        aggregateHeight = 0
+        columnHeights = [0] * cols
+        holes = 0
+
+        for c in range(cols):
+            blockFound = False
+
+            for r in range(rows):
+                if board[r][c] == 1:
+                    if not blockFound:
+                        h = rows - r
+                        columnHeights[c] = h
+                        aggregateHeight += h
+                        blockFound = True
+                elif blockFound and board[r][c] == 0:
+                    holes += 1
+
+        bumpiness = 0
+
+        #sum of absolute difference between adjacent column height
+        for c in range(cols - 1):
+            bumpiness += abs(columnHeights[c] - columnHeights[c+1])
+
+        score = (
+            HEIGHT_WEIGHT * aggregateHeight
+            + HOLES_WEIGHT * holes
+            + BUMPINESS_WEIGHT * bumpiness
+            + LINES_WEIGHT * currGameState.lines
+        )
+
+        return score
+
